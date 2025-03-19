ee0663d9af207c3f156926684c5a464d3970af0b
Beatriz Lagos <beatrizlagosn@gmail.com>
2025-03-18 20:09:55 -0500

    agregar validación para nombres vacíos

--- app.js
+++ app.js
@@ -2,9 +2,24 @@
 
 let amigos = [];
 
+const validarNombre = (nombre) => {
+  if (nombre === "") {
+    alert("El nombre no puede estar vacío");
+    return false;
+  }
+
+  return true;
+};
+
 const agregarAmigo = () => {
   const inputDelNombre = document.getElementById("amigo");
   const nombre = inputDelNombre.value;
+
+  const esUnNombreValido = validarNombre(nombre);
+
+  if (!esUnNombreValido) {
+    return;
+  }
 
   amigos.push(nombre);
 
